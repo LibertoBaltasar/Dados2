f2f3cae934aa2d861f3881fbad6602054182e072
Liberto Baltasar Mendez <liberto.baltasar@gmail.com>
2025-02-01 21:08:26 +0100

    V5 terminada

--- app/src/main/java/com/example/dados2/videoplayer/VideoPlayerActivity.kt
+++ app/src/main/java/com/example/dados2/videoplayer/VideoPlayerActivity.kt
@@ -2,10 +2,10 @@ package com.example.dados2.videoplayer
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
 import android.widget.VideoView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -35,6 +35,9 @@ fun VideoPlayer(uri: String, modifier: Modifier = Modifier) {
         factory = { context ->
             VideoView(context).apply {
                 setVideoURI(Uri.parse(uri))
+                val mediaController = MediaController(context)
+                mediaController.setAnchorView(this)
+                setMediaController(mediaController)
                 start()
             }
         }
